cbf92083d005627910d3e44c380441f5fd600ad8
MOVEEDU\alexandre.muniz <xanlex.muniz@gmail.com>
2025-02-27 19:51:45 -0300

    Meu primeiro commit

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function calcularidade(){
+    let anonascimento = document.getElementById("anonascimento").value;
+    let idade = 2025 - anonascimento;
+    //let retorno = String("A sua idade é" + idade);
+    //document.getElementById("resultado").innerHTML = retorno;
+
+    document.getElementById("resultado").innerHTML = `idade é : ${idade}`;
+
+}
